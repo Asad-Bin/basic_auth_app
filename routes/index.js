51073a671471d210937480832a8b11145095101a
Asad Bin Saber <mdasad.binsaber@gmail.com>
2025-02-28 20:23:14 +0600

    add lead show edit and post routes

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,9 @@ router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
 router.get('/leads', landing.show_leads);
 router.get('/lead/:lead_id', landing.show_lead);
+// for purpose of editing form for lead_id
+router.get('/lead/:lead_id/edit', landing.show_edit_lead);
+// for submitting form for lead_id
+router.post('/lead/:lead_id/edit', landing.edit_lead);
 
 module.exports = router;
