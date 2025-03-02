fde4781be60c3412a5f9fd374a0d2e91331fb494
Asad Bin Saber <mdasad.binsaber@gmail.com>
2025-03-02 22:49:10 +0600

    some basic updates

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var logger = require('morgan');
 
 let passport = require('passport');
 let session = require('express-session');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+let flash = require('connect-flash')
 
 require('./passport_setup')(passport);
 var app = express();
@@ -17,13 +17,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(flash())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({secret: 'our new secret'}));
+app.use(session({ secret: 'our new secret'}));
 app.use(passport.initialize());
 app.use(passport.session());
 
